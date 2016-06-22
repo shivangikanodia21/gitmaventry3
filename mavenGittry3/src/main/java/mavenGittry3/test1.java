d9da4ddb4af1b7b51df712c9624336c5960d3a1e
shivangi.kanodia <shivangi.kanodia@BDC4A-D-74MSX52.dir.svc.accenture.com>
2016-06-22 12:49:03 +0530

    commit 1.0

---
+++ mavenGittry3/src/main/java/mavenGittry3/test1.java
@@ -0,0 +1,21 @@
+package mavenGittry3;
+
+
+import org.testng.annotations.Test;
+
+public class test1 {
+
+	@Test
+	public void openBrowser()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("test started");
+		//WebDriver wd=new FirefoxDriver();
+		//wd.get("http://www.w3schools.com/html/html_tables.asp");
+		//System.out.println(wd.getTitle());
+		System.out.println("test completed");
+
+	}
+
+
+}
